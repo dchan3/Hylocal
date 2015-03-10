8387de5d281faa4526db88556cbbf800f34ee061
dchan3 <sunquan8094@dhcp-168-105-222-250.wireless.manoa.hawaii.edu>
2015-03-10 13:26:40 -1000

    Main file change

--- src/hylocal/Main.java
+++ src/hylocal/Main.java
@@ -1,9 +1,16 @@
 package hylocal;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String args[]) {
-		int[][] dates = {{2015, 5, 8} , {2015, 5, 8}};
-		int[][] times = {{18, 0, 0}, {20,0,0}};
-		ICSCreator.createICSFile("N-Genir", 0, "Hamilton Library", 9, "Studying for finals", dates, times);
+		// int[][] dates = {{2015, 5, 8} , {2015, 5, 8}};
+		// int[][] times = {{18, 0, 0}, {20,0,0}};
+		// ICSCreator.createICSFile("N-Genir", 0, "Hamilton Library", 9, "Studying for finals", dates, times);
+	}
+	
+	public static void takeInput() {
+		Scanner in = new Scanner(System.in);
+		String fn = "", loc = "", sum = "";
 	}
 }
